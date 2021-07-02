fa74362f197fbfc74468b8938a3f39458c9ed63f
allendlg <allen.delagarza01@gmail.com>
2021-07-01 23:37:02 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+results= function(element) {
+    return (element.result==="W") 
 
+}
+function superbowlWin(array) {
+    const dub= array.find(results)
+    if (dub===undefined) {
+        return undefined;
+    } else {
+    return dub.year;
+    }
+}
